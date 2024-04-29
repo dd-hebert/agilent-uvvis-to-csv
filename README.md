a30b0973caf1edb085cc7d1b47433d5b74a175b3
David Hebert <39543390+dd-hebert@users.noreply.github.com>
2024-04-28 21:50:38 -0400

    Minor update 0.1.9 -> 0.1.10
    Added typehints, minor refactoring.
    
    BinaryFile has been renamed to UVvisFile.
    
    The `wavelength_range` param has been made more flexible: min and max wavelength can be given in any order.

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # uv2csv
-A simple script for converting Agilent 845x Chemstation UV-Vis binary files (**.KD** or **.SD** formats) to .csv format.
+A simple script for converting Agilent 845x Chemstation UV-vis binary files (**.KD** or **.SD** formats) to .csv format.
 
-Do you have **.KD** or **.SD** UV-Vis files but no access to the Agilent 845x UV-Vis Chemstation software? Do you have large **.KD** files that contain thousands of spectra? This script will automatically convert your **.KD** or **.SD** files to .csv format (and it's significantly faster than the Agilent Chemstation software!). Exports thousands of spectra in seconds!
+Do you have **.KD** or **.SD** UV-vis files but no access to the Agilent 845x UV-vis Chemstation software? Do you have large **.KD** files that contain thousands of spectra? This script will automatically convert your **.KD** or **.SD** files to .csv format (and it's significantly faster than the Agilent Chemstation software!). Exports thousands of spectra in seconds!
 
-Currently supports **.KD** and **.SD** UV-Vis binary file types.
+Currently supports **.KD** and **.SD** UV-vis binary file types.
 
 ## Using This Script
 To use this script, simply run it from the command line:
@@ -18,18 +18,18 @@ This script works by searching a **.KD** or **.SD** file for a unique byte strin
 
 ## Export Location for .CSV Files
 You can locate the exported .csv files in the same directory as the original **.KD** or **.SD** binary file.
-- **.KD/.SD Files with Multiple Spectra:** .csv files are exported into a folder within the same directory as the binary file.
-- **.KD/.SD Files with a Single Spectrum:** A single .csv file is exported to the same directory as the binary file.
+- **Files with Multiple Spectra:** .csv files are exported into a folder within the same directory as the binary file.
+- **Files with a Single Spectrum:** A single .csv file is exported to the same directory as the binary file.
 
-**Note:** If you are parsing a **.SD** file which contains sample names, the sample names will be included in the file names of the exported .csv files. Please be aware that sample names are not currently supported for **.KD** files.
+**Note:** If the **.SD** file you are parsing contains sample names, the sample names will be included in the file names of the exported .csv files. Please be aware that sample names are not currently supported for **.KD** files.
 
 ## Changing The Wavelength Range
-By default, the script assumes your spectrometer captures data from **190 nm to 1100 nm**. If the range of wavelengths your spectrometer records is different than this, you can set the wavelength range by adding a single keyword argument ``wavelength_range`` to the ``BinaryFile`` object constructor at the bottom of the script:
+By default, the script assumes your spectrometer captures data from **190 nm to 1100 nm**. If the range of wavelengths your spectrometer records is different than this, you can set the wavelength range by adding a single keyword argument ``wavelength_range`` to the ``UVvisFile`` object constructor at the bottom of the script:
 
 ```python
 if __name__ == '__main__':
     PATH = os.path.normpath(input('Enter a file path or "q" to quit: '))
-    BINARY_FILE = BinaryFile(PATH, export_csv=True, wavelength_range=(min, max))
+    UV_FILE = UVvisFile(PATH, export_csv=True, wavelength_range=(min, max))
 
 ```
 
@@ -37,7 +37,7 @@ if __name__ == '__main__':
 You must have the [pandas](https://pandas.pydata.org/) python package installed to use this script.
 
 ## Disclaimer
-This script has only been tested on files from a small number of different versions of the Agilent UV-Vis Chemstation software. Therefore, binary files from other versions of UV-Vis Chemstation, different spectrometer setups, or custom methods may not work.
+This script has only been tested on files from a small number of different versions of the Agilent UV-vis Chemstation software. Therefore, binary files from other versions of UV-vis Chemstation, different spectrometer setups, or custom methods may not work.
 
 **Known Supported Chemstation Versions:**
 - B.05.02 [16]
--- uv2csv.py
+++ uv2csv.py
@@ -1,10 +1,10 @@
 """
-A simple script to convert Agilent 845x Chemstation UV-Vis files to .csv format.
+A simple script to convert Agilent 845x Chemstation UV-vis files to .csv format.
 
 To use this script, simply run it from the command line then provide a file
-path. Currently supported UV-Vis file types are .KD and .SD files.
+path. Currently supported UV-vis file types are .KD and .SD files.
 
-Version 0.1.9
+Version 0.1.10
 Created by David Hebert
 
 """
@@ -14,30 +14,30 @@ import struct
 import pandas as pd
 
 
-class BinaryFile:
+class UVvisFile:
     """
-    A BinaryFile object.
+    A UVvisFile object.
 
-    Supported file types are .KD and .SD. Upon creation of a BinaryFile object,
-    the specified .KD or .SD binary file is read and unpacked into a
+    Supported file types are .KD and .SD. Upon creation of a UVvisFile object, \
+    the specified .KD or .SD binary file is read and unpacked into a \
     :class:`pandas.DataFrame`.
     """
 
     supported_file_types = ['.KD', '.SD']
 
-    def __init__(self, path, export_csv=False, wavelength_range=(190, 1100)):
+    def __init__(self, path: str, export_csv: bool = False, wavelength_range: tuple = (190, 1100)) -> None:
         """
-        Initialize a :class:`BinaryFile` object and read a UV-Vis binary file.
+        Initialize a :class:`UVvisFile` object and read a UV-vis binary file.
 
         Parameters
         ----------
         path : str
-            The file path to a UV-Vis binary file (.KD or .SD format).
+            The file path to a UV-vis binary file (.KD or .SD format).
         export_csv : bool
-            True or False. Set to True to automatically export .csv files upon creation
-            of the :class:`BinaryFile` object. Default is False (.csv files not automatically
+            True or False. Set to True to automatically export .csv files upon creation \
+            of the :class:`UVvisFile` object. Default is False (.csv files not automatically \
             exported.)
-        wavelength_range : tuple
+        wavelength_range : tuple[int, int]
             Set the range of wavelengths (in nm) recorded by the detector (min, max).
             Default value is (190, 1100).
 
@@ -49,14 +49,13 @@ class BinaryFile:
         self._check_path()
         if self.path:
             self.wavelength_range = wavelength_range
-            self._check_wavelength_range()
             self.absorbance_table_length = self._absorbance_table_length()
             self.name, self.file_type = os.path.splitext(os.path.basename(self.path))
             self.spectra, self.samplenames = self.read_binary()
             if export_csv is True:
                 self.export_csv()
 
-    def _check_path(self):
+    def _check_path(self) -> None:
         while True:
             if self.path.lower() == 'q':
                 self.path = ''
@@ -69,25 +68,21 @@ class BinaryFile:
             print('Invalid file path (must be a .KD or .SD file).')
             self.path = os.path.normpath(input('Enter a file path or "q" to quit: '))
 
-    def _check_wavelength_range(self):
-        if self.wavelength_range[0] > self.wavelength_range[1]:
-            raise ValueError('Wavelength range error: minimum wavelength greater than maximum.')
-
-    def _absorbance_table_length(self):
+    def _absorbance_table_length(self) -> int:
         # Data is 8 hex characters per wavelength long.
-        absorbance_table_length = (self.wavelength_range[1] - self.wavelength_range[0]) * 8 + 8
-        return absorbance_table_length
+        absorbance_table_length = abs(self.wavelength_range[1] - self.wavelength_range[0]) * 8 + 8
+        return int(absorbance_table_length)
 
-    def read_binary(self):
+    def read_binary(self) -> tuple[list[pd.DataFrame], list[str]]:
         """
         Read a .KD or .SD file and extract spectra and sample names.
 
         Returns
         -------
-        spectra : list of :class:`pandas.DataFrame` objects
-            A list of :class:`pandas.DataFrame` objects containing the UV-Vis
+        spectra : list[pd.DataFrame]
+            A list of :class:`pandas.DataFrame` objects containing the UV-vis
             spectra.
-        samplenames : list
+        samplenames : list[str]
             A list of strings with sample names.
         """
         print(f'Reading {self.file_type} file...')
@@ -102,7 +97,7 @@ class BinaryFile:
             return spectra, samplenames
         return spectra, [''] * len(spectra)
 
-    def _read_spectra(self, file_bytes):
+    def _read_spectra(self, file_bytes: bytes) -> list[pd.DataFrame]:
         """
         Read the spectra from a .KD or .SD file.
 
@@ -118,13 +113,13 @@ class BinaryFile:
 
         Returns
         -------
-        spectra : list of :class:`pandas.DataFrame` objects
-            A list of :class:`pandas.DataFrame` objects containing the UV-Vis
+        spectra : list[pd.DataFrame]
+            A list of :class:`pandas.DataFrame` objects containing the UV-vis
             spectra.
         """
         spectra = []
         position = 0
-        wavelength = list(range(self.wavelength_range[0], self.wavelength_range[1] + 1))
+        wavelength = list(range(min(self.wavelength_range), max(self.wavelength_range) + 1))
         absorbance_data_header, spacing = self._find_absorbance_data_header(file_bytes)
 
         while True:
@@ -132,20 +127,19 @@ class BinaryFile:
             if header_location == -1:
                 break
 
-            data_start = header_location + spacing
-            data_end = data_start + self.absorbance_table_length
-            absorbance_data = file_bytes[data_start:data_end]
-            absorbance_values = [value for value, in struct.iter_unpack('<d', absorbance_data)]
+            start = header_location + spacing
+            end = start + self.absorbance_table_length
+            absorbance_values = [val for val, in struct.iter_unpack('<d', file_bytes[start:end])]
             spectra.append(pd.DataFrame({'Wavelength (nm)': wavelength,
                                          'Absorbance (AU)': absorbance_values}))
-            position = data_end
+            position = end
 
         if spectra == []:
             raise Exception('Error parsing file. No spectra found.')
 
         return spectra
 
-    def _find_absorbance_data_header(self, file_bytes):
+    def _find_absorbance_data_header(self, file_bytes: bytes) -> tuple[bytes, int]:
         """
         Search the binary file for the appropriate absorbance data header.
 
@@ -161,7 +155,7 @@ class BinaryFile:
 
         Returns
         -------
-        header : string
+        header : bytes
             The absorbance data header as a hex string.
         spacing : int
             The spacing between the header and the beginning of the actual data.
@@ -169,13 +163,16 @@ class BinaryFile:
         # Each header contains its hex string and the spacing that follows it.
         headers = {
             '( A U ) ': (b'\x28\x00\x41\x00\x55\x00\x29\x00', 17),
-            '(AU) ': (b'\x28\x41\x55\x29\x00', 5)}
-        for key, (header, spacing) in headers.items():
+            '(AU) ': (b'\x28\x41\x55\x29\x00', 5)
+        }
+
+        for _, (header, spacing) in headers.items():
             if file_bytes.find(header, 0) != -1:
                 return header, spacing
+
         raise Exception('Error parsing file. No absorbance data headers could be found.')
 
-    def _read_samplenames(self, file_bytes, num_samples):
+    def _read_samplenames(self, file_bytes: bytes, num_samples: int) -> list[str]:
         """
         Read the sample names from a .SD file.
 
@@ -188,8 +185,8 @@ class BinaryFile:
 
         Returns
         -------
-        samplenames : list
-            A list of strings with the sample names.
+        samplenames : list[str]
+            A list of sample names as strings.
         """
         samplename_header, spacing, end_char = self._find_samplename_header(file_bytes)
         if samplename_header is None:
@@ -207,10 +204,9 @@ class BinaryFile:
             if find_end_char == -1:
                 break
 
-            samplename_start = find_header + spacing
-            samplename_end = find_end_char
-            raw_samplename = file_bytes[samplename_start:samplename_end]
-            samplename = raw_samplename.replace(b'\x00', b'').decode('ascii', 'replace')
+            start = find_header + spacing
+            end = find_end_char
+            samplename = file_bytes[start:end].replace(b'\x00', b'').decode('ascii', 'replace')
             samplenames.append(samplename)
             position = find_end_char + self.absorbance_table_length
 
@@ -221,9 +217,10 @@ class BinaryFile:
 
         return samplenames
 
-    def _find_samplename_header(self, file_bytes):
+    def _find_samplename_header(self, file_bytes: bytes) -> tuple[bytes, int, bytes] | tuple[None, None, None]:
         """
-        Search the binary file for the appropriate sample name header.
+        Search the binary file for the appropriate sample name header. Returns (`None`, `None`, `None`) \
+        if a header is not found.
 
         Parameters
         ----------
@@ -232,31 +229,35 @@ class BinaryFile:
 
         Returns
         -------
-        header : string
+        header : bytes or None
             The sample name header as a hex string.
-        spacing : int
+        spacing : int or None
             The spacing between the header and the beginning of the actual data.
-        end_char : bytes
+        end_char : bytes or None
             The end-of-data termination character.
         """
         # Each header contains its hex string, spacing, and termination character.
         headers = {
             'S a m p l e N a m e ': (b'\x53\x00\x61\x00\x6D\x00\x70\x00\x6C\x00\x65\x00\x4E\x00\x61\x00\x6D\x00\x65\x00', 28, b'\x09'),
-            '(`DataType': (b'\x28\x60\x44\x61\x74\x61\x54\x79\x70\x65', 33, b'\x02')}
-        for key, (header, spacing, end_char) in headers.items():
+            '(`DataType': (b'\x28\x60\x44\x61\x74\x61\x54\x79\x70\x65', 33, b'\x02')
+        }
+
+        for _, (header, spacing, end_char) in headers.items():
             if file_bytes.find(header, 0) != -1:
                 return header, spacing, end_char
+
         return None, None, None
 
-    def export_csv(self):
+    def export_csv(self) -> None:
         """
         Export spectra as .csv files.
 
-        For files containing multiple spectra, .csv files are exported to a
-        folder named ``self.name`` in ``self.path``. For files with a single
-        spectrum, a .csv file named ``self.name`` is exported to ``self.path``.
+        For files containing multiple spectra, .csv files are exported to a \
+        folder named ``self.name`` in ``self.path``. For files with a single \
+        spectrum, a single .csv file named ``self.name`` is exported to \
+        ``self.path``.
 
-        For .SD files, if a spectrum has a sample name associated with it, the
+        For .SD files, if a spectrum has a sample name associated with it, the \
         sample name will be added to the filename as well.
 
         Returns
@@ -265,7 +266,7 @@ class BinaryFile:
         """
         print('Exporting .csv files...')
 
-        def get_unique_directory(path):
+        def get_unique_directory(path: str) -> str:
             """If a folder named self.name exists, add a number after."""
             n = 1
             output_dir = path
@@ -274,7 +275,7 @@ class BinaryFile:
                 n += 1
             return output_dir
 
-        def get_unique_filename(base_filename):
+        def get_unique_filename(base_filename: str) -> str:
             """If a file named base_filename exists, add a number after."""
             n = 1
             unique_filename = base_filename
@@ -308,4 +309,4 @@ class BinaryFile:
 
 if __name__ == '__main__':
     PATH = os.path.normpath(input('Enter a file path or "q" to quit: '))
-    BINARY_FILE = BinaryFile(PATH, export_csv=True)
+    UV_FILE = UVvisFile(PATH, export_csv=True)
